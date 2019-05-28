bdb9433b882330646e55f28c50fffa722c15f046
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-05-28 14:23:39 +0100

    Wrote first shot of Tonelli-Shanks algorithm

--- gfp2.go
+++ gfp2.go
@@ -2,6 +2,7 @@ package bn256
 
 import (
 	"math"
+	"errors"
 	"math/big"
 )
 
@@ -131,9 +132,14 @@ func (e *gfP2) MulXi(a *gfP2) *gfP2 {
 func (e *gfP2) Square(a *gfP2) *gfP2 {
 	// Complex squaring algorithm:
 	// (xi+y)² = (x+y)(y-x) + 2*i*x*y
+	// - "Devegili OhEig Scott Dahab --- Multiplication and Squaring on Pairing-Friendly Fields.pdf"; Section 3 (Complex squaring)
+	// - URL: https://eprint.iacr.org/2006/471.pdf
+	// Here, since the non residue used is β = -1 in Fp, then we have:
+	// c0 = (a0 + a1)(a0 + βa1) - v0 - βv0 => c0 = (a0 + a1)(a0 - a1)
+	// c1 = 2v0, where v0 is a0 * a1 (= x * y, with our notations)
 	tx, ty := &gfP{}, &gfP{}
-	gfpSub(tx, &a.y, &a.x)
-	gfpAdd(ty, &a.x, &a.y)
+	gfpSub(tx, &a.y, &a.x) // a.y - a.x
+	gfpAdd(ty, &a.x, &a.y) // a.x + a.y
 	gfpMul(ty, tx, ty)
 
 	gfpMul(tx, &a.x, &a.y)
@@ -215,4 +221,88 @@ func (e *gfP2) Exp(exponent *big.Int) *gfP2 {
 
 	e.Set(res)
 	return e
+}
+
+// Sqrt returns the square root of e in GFp2
+// See: 
+// - "A High-Speed Square Root Algorithm for Extension Fields - Especially for Fast Extension Fields"
+// - URL: https://core.ac.uk/download/pdf/12530172.pdf
+//
+// - "Square Roots Modulo p"
+// - URL: http://www.cmat.edu.uy/~tornaria/pub/Tornaria-2002.pdf
+//
+// - "Faster square roots in annoying finite fields"
+// - URL: http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.21.9172
+func (e *gfP2) Sqrt() *gfP2 {
+	// In GF(p^m), Euler's Criterion is defined like EC(x) = x^((p^m -1) / 2), if EC(x) == 1, x is a QR; if EC(x) == -1, x is a QNR
+	// `euler` here, is the exponent used in Euler's criterion, thus, euler = (p^m -1) / 2 for GF(p^m)
+	// here, we work over GF(p^2), so euler = (p^2 -1) / 2, where p = 21888242871839275222246405745257275088696311157297823662689037894645226208583
+	////euler := bigFromBase10("239547588008311421220994022608339370399626158265550411218223901127035046843189118723920525909718935985594116157406550130918127817069793474323196511433944")
+
+	// modulus^2 = 2^s * t + 1 => p^2 = 2^s * t + 1, where t is odd
+	// In our case, p^2 = 2^s * t + 1, where s = 4, t = 29943448501038927652624252826042421299953269783193801402277987640879380855398639840490065738714866998199264519675818766364765977133724184290399563929243
+	////t := bigFromBase10("29943448501038927652624252826042421299953269783193801402277987640879380855398639840490065738714866998199264519675818766364765977133724184290399563929243")
+	//s := bigFromBase10("4")
+	s := 4
+
+	// tMinus1Over2 = (t-1) / 2
+	tMinus1Over2 := bigFromBase10("14971724250519463826312126413021210649976634891596900701138993820439690427699319920245032869357433499099632259837909383182382988566862092145199781964621")
+
+	// A non quadratic residue in Fp
+	////nonResidueFp := bigFromBase10("21888242871839275222246405745257275088696311157297823662689037894645226208582")
+
+	// A non quadratic residue in Fp2. Here nonResidueFp2 = i + 2 (Euler Criterion applied to this element of Fp2 shows that this element is a QNR)
+	////nonResidueFp2 := &gfP2{*newGFp(1), *newGFp(2)}
+
+	// nonResidueFp2 ^ t
+	nonResidueFp2ToTXCoord := bigFromBase10("314498342015008975724433667930697407966947188435857772134235984660852259084")
+	nonResidueFp2ToTYCoord := bigFromBase10("5033503716262624267312492558379982687175200734934877598599011485707452665730")
+	nonResidueFp2ToT := &gfP2{*newMontEncodedGFpFromBigInt(nonResidueFp2ToTXCoord), *newMontEncodedGFpFromBigInt(nonResidueFp2ToTYCoord)}
+
+	// Start algorithm
+	// Initialize the algorithm variables
+	v := s
+	z := nonResidueFp2ToT
+	w := new(gfP2).Set(e)
+	w = w.Exp(tMinus1Over2)
+	x := new(gfP2).Mul(e, w)
+	b := new(gfP2).Mul(x, w) // contains e^t
+	
+	// Check if the element is a QR
+	// Since p^2 = 2^s * t + 1 => t = (p^2 - 1)/2
+	// Thus, since we have b = e^t, and since we want to test if e is a QR
+	// we need to square b (s-1) times. That way we'd have
+	// (e^t)^{2^(s-1)} which equals e^{(p^2 - 1)/2} => Euler criterion
+	bCheck := new(gfP2).Set(b)
+	for i := 0; i < 3; i++ { // 3 == s-1 here, to replace by a variable to make it cleaner (see comment above)
+        bCheck = bCheck.Square(bCheck);
+    }
+    if !bCheck.IsOne() {
+        panic(errors.New("Cannot extract a root. The element is not a QR in Fp2"))
+	}
+
+	// Extract the root of the quadratic residue using the Tonelli-Shanks algorithm
+	for !b.IsOne() {
+        m := 0
+        b2m := new(gfP2).Set(b)
+        for (!b2m.IsOne()) {
+            /* invariant: b2m = b^(2^m) after entering this loop */
+            b2m = b2m.Square(b2m)
+            m++
+        }
+
+        j := v-m-1
+        w = z
+        for j > 0 {
+            w = w.Square(w)
+            j--
+        } // w = z^2^(v-m-1)
+
+        z = new(gfP2).Square(w)
+        b = b.Mul(b, z)
+        x = x.Mul(x, w)
+        v = m
+    }
+
+    return x
 }
